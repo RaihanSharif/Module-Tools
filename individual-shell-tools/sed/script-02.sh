df0e647baf9706aef41278202511fcdb974048d4
Raihan Sharif <raihansharif92@gmail.com>
2026-07-10 16:40:33 +0100

    sed2

--- individual-shell-tools/sed/script-02.sh
+++ individual-shell-tools/sed/script-02.sh
@@ -5,3 +5,7 @@ set -euo pipefail
 # TODO: Write a command to output input.txt with numbers removed.
 # The output should contain 11 lines.
 # Line 6 of the output should be " Alisha".
+
+# -E is extended regex, needed for + (one or more) quantifier
+# [[:space:]] is POSIX compliant
+sed -E s':[0-9]+[[:space:]]::g' ./input.txt
