05409ac41c30c34c194ae56d2f483eeeeeeaee16
Raihan Sharif <raihansharif92@gmail.com>
2026-07-10 17:51:04 +0100

    wc3

--- individual-shell-tools/wc/script-03.sh
+++ individual-shell-tools/wc/script-03.sh
@@ -8,3 +8,5 @@ set -euo pipefail
 #        1       7      39 ../helper-files/helper-2.txt
 #        3      19      92 ../helper-files/helper-3.txt
 #        5      30     151 total
+
+wc -lwc ../helper-files/helper-1.txt ../helper-files/helper-2.txt ../helper-files/helper-3.txt
