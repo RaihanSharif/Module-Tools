f39a089a1b9458fb45c80668df4304db265f0d71
Raihan Sharif <raihansharif92@gmail.com>
2026-07-10 17:12:47 +0100

    sed3

--- individual-shell-tools/sed/script-03.sh
+++ individual-shell-tools/sed/script-03.sh
@@ -4,3 +4,12 @@ set -euo pipefail
 
 # TODO: Write a command to output input.txt removing any line which contains a number.
 # The output should contain 6 lines.
+
+# Should this not be done with grep?
+
+# -n prevent default printing behaviour 
+# ! negates regex match, i.e. gets lines that don't match
+# p prints
+sed -n ':[0-9]:!p' input.txt
+
+
