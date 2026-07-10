b958808bf3e12841b4f4ce302ebef2169a6c94ab
Raihan Sharif <raihansharif92@gmail.com>
2026-07-10 17:36:50 +0100

    sed5

--- individual-shell-tools/sed/script-05.sh
+++ individual-shell-tools/sed/script-05.sh
@@ -6,3 +6,10 @@ set -euo pipefail
 # If a line starts with a number and a space, make the line instead end with a space and the number.
 # So line 6 which currently reads "37 Alisha" should instead read "Alisha 37".
 # The output should contain 11 lines.
+
+# Use capture groups
+# 1 is just numbers
+# space after number is part of the pattern but not the group 
+# 2 is everything else 
+# then print out group 2, a space, and then group 1
+sed -E 's/^([0-9]+) (.*)/\2 \1/' input.txt
