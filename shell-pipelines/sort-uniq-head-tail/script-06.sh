3facc5721ec0b8dfe416b5719c87e6f2c4136a22
Raihan Sharif <raihansharif92@gmail.com>
2026-07-22 13:51:31 +0100

    sort-uniq-head

--- shell-pipelines/sort-uniq-head-tail/script-06.sh
+++ shell-pipelines/sort-uniq-head-tail/script-06.sh
@@ -5,3 +5,10 @@ set -euo pipefail
 # The input for this script is the events.txt file.
 # TODO: Write a command to show how many times anyone has entered and exited.
 # It should be clear from your script's output that there have been 5 Entry events and 4 Exit events.
+
+# v1
+#entryCount=$(grep -c '^Entry' events.txt)
+#exitCount=$(grep -c '^Exit' events.txt)
+#echo "There were $entryCount entries, $exitCount exits"
+
+cut -d' ' -f1 ./events.txt | sort | uniq -c
