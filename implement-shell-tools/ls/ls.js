4d0e873beba76178fdb2f0610835e6afb4bd2337
Raihan Sharif <raihansharif92@gmail.com>
2026-07-23 17:09:43 +0100

    working subdirectories

--- implement-shell-tools/ls/ls.js
+++ implement-shell-tools/ls/ls.js
@@ -1,26 +1,53 @@
+import { dir } from "node:console";
 import fs from "node:fs";
+import path from "node:path";
 import process from "node:process";
 
 const args = process.argv.slice(2);
 
 const flags = new Set();
-const path = "";
+let paths = [];
+
+if (args.length === 0) {
+    args.push(".");
+}
 
 for (const arg of args) {
     if (arg.startsWith("-") && arg !== "-") {
         // capture the flags without the -
+        // supports combined flags like -1a
         for (const ch of arg.slice(1)) {
             flags.add(ch);
         }
+    } else {
+        paths.push(arg);
     }
 }
 
-console.log(flags);
+const files = [];
+const directories = [];
 
-// console.log(
-//     fs.readdir("./", (err, files) => {
-//         if (err) throw err;
+if (paths.length > 1) {
+    paths.forEach((path) => {
+        if (fs.statSync(path).isDirectory()) {
+            directories.push(path);
+        } else {
+            files.push(path);
+        }
+    });
+} else {
+    fs.readdir(paths[0], (err, files) => console.log(files.join("\t")));
+}
 
-//         console.log(files);
-//     }),
-// );
+if (files.length > 0) {
+    console.log(files.join("\t"));
+}
+
+if (directories.length > 0) {
+    directories.forEach((dir) => {
+        console.log(`\n${dir}:`);
+        fs.readdir(dir, (err, files) => {
+            console.log(files.join("\t"));
+        });
+    });
+}
