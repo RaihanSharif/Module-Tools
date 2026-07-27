45766e070bf45ecc94d75f7cb7d820e95b13efe3
Raihan Sharif <raihansharif92@gmail.com>
2026-07-27 09:35:32 +0100

    parse file arguments

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -1,14 +1,18 @@
 import { program } from "commander";
+import fs from "node:fs";
+import process from "node:process";
 
 program
     .name("wc")
     .description("Mimics the wc command line tool")
     .option("-w")
     .option("-c")
-    .option("-l");
+    .option("-l")
+    .argument("<files...>", "file to process");
 
 program.parse();
 
 const options = program.opts();
+const files = program.args;
 
-console.log(options);
+console.log(files);
