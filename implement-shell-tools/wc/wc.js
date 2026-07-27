2c3458fcbae16c0d9fa4c33733c94aee0d27e3e8
Raihan Sharif <raihansharif92@gmail.com>
2026-07-27 09:58:23 +0100

    handle no flag arguments

--- implement-shell-tools/wc/wc.js
+++ implement-shell-tools/wc/wc.js
@@ -15,4 +15,19 @@ program.parse();
 const options = program.opts();
 const files = program.args;
 
+console.log(options);
 console.log(files);
+
+// if no -lwc flags are supplied, wc prints
+// lines, words, bytes of each file
+// whereas if any flags are supplied only those
+// values are printed
+if (Object.keys(options).length === 0) {
+    options.l = options.w = options.c = true;
+}
+
+/*
+array of objects with data [{l: 2, w: 12: c: 123, file: 'sample-files/1.txt}, ...]
+For each item, create a temp string
+If options.l, append to temp string the w value
+*/
