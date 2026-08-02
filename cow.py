eaf7f8d3dd9faabc8194f20eb2f0d592803c35fc
Raihan Sharif <raihansharif92@gmail.com>
2026-08-02 17:00:13 +0100

    completed

--- cow.py
+++ cow.py
@@ -3,13 +3,27 @@ import sys
 import argparse
 
 
-#  parse the args
+#  set up options and argument parser
 parser = argparse.ArgumentParser(
     prog="cowsay command line tool",
     description="Make animals say things")
 
+# takes an --animal option, which takes a limited set of argument - names of characters in cowsay
 parser.add_argument("--animal", choices=cowsay.char_names, help="The animal to be saying things.")
+
+# nargs takes a list of multiple (n) args to a list then perform one action. '*' means multiple 
+# optional arguments, all collected into the message option since this is defined after the --animal option, 
+# it will gather everything after the argument for the animal option
 parser.add_argument("message", nargs='*', help="The message to say")
 
+# parses the args into a Namepace class, e.g. Namespace(animal='fox', message=['hello,', 'starfox'])
 args = parser.parse_args()
-# cowsay.cow(" ".join(sys.argv[1:]))
+
+# extract the aimal argument
+animal = vars(args)['animal']
+
+# list of positional arguments as space-separated string
+message = " ".join(vars(args)['message'])
+
+# call cowsay with the animal and message
+print(cowsay.get_output_string(animal, message))
