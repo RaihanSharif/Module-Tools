6cac76e85f8619900998305043aa67ee13e3c764
Raihan Sharif <raihansharif92@gmail.com>
2026-08-02 17:30:18 +0100

    tidier implementation

--- implement-cowsay/cow.py
+++ implement-cowsay/cow.py
@@ -9,24 +9,18 @@ parser = argparse.ArgumentParser(
     description="Make animals say things")
 
 # takes an --animal option, which takes a limited set of argument - names of characters in cowsay
-parser.add_argument("--animal", choices=cowsay.char_names, help="The animal to be saying things.")
+parser.add_argument("--animal", choices=cowsay.char_names, help="The animal to be saying things.", default="cow")
 
 # nargs takes a list of multiple (n) args to a list then perform one action. '*' means multiple 
 # optional arguments, all collected into the message option since this is defined after the --animal option, 
 # it will gather everything after the argument for the animal option
-parser.add_argument("message", nargs='*', help="The message to say")
+parser.add_argument("message", nargs="*", help="The message to say")
 
 # parses the args into a Namepace class, e.g. Namespace(animal='fox', message=['hello,', 'starfox'])
 args = parser.parse_args()
 
-# extract the aimal argument
-animal = vars(args)['animal']
-
 # list of positional arguments as space-separated string
-message = " ".join(vars(args)['message'])
+message = " ".join(args.message)
 
 # call cowsay with the animal and message
-if (animal):
-    print(cowsay.get_output_string(animal, message))
-else:
-    cowsay.cow(message)
+print(cowsay.get_output_string(args.animal, message))
