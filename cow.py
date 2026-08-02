5ff78fcf905651dc4e82c3f33dcc5c89e2ba1b8b
Raihan Sharif <raihansharif92@gmail.com>
2026-08-02 16:22:31 +0100

    --animal args limited to cowsay provided options
    Any invalid argument for the flag will show usage message

--- cow.py
+++ cow.py
@@ -1,4 +1,14 @@
 import cowsay
 import sys
+import argparse
 
-cowsay.cow(" ".join(sys.argv[1:]))
+
+#  parse the args
+parser = argparse.ArgumentParser(
+    prog="cowsay command line tool",
+    description="ASCII art of animals saying text supplied as argument")
+
+parser.add_argument("--animal", choices=cowsay.char_names)
+
+args = parser.parse_args()
+# cowsay.cow(" ".join(sys.argv[1:]))
