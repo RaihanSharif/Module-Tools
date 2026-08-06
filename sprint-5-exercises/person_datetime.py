9044ad56894fab69c406f502c05b5efbbb91f49b
Raihan Sharif <raihansharif92@gmail.com>
2026-08-06 20:26:33 +0100

    account for leap year

--- sprint-5-exercises/person_datetime.py
+++ sprint-5-exercises/person_datetime.py
@@ -11,7 +11,18 @@ class Person:
 
     def is_adult(self) -> bool:
         today = dt.date.today()
-        print(today)
+        years = today.year - self.birthdate.year
+        # python does a lexicographical comparison of the elements in the tuples
+        # only checks the days if the months are equal
+
+        had_birthday_this_year = (today.month, today.day) >= (self.birthdate.month, self.birthdate.day)
+        age = years if had_birthday_this_year else years - 1
+        return age >= 18
+
+        # note: the above is necessary because with my old version, if the original birthday is on feb 29
+        # then it would try to create a new date of feb 29 on a non-leap year and crash
+
+
         return today >= dt.date(self.birthdate.year +18, self.birthdate.month, self.birthdate.day)
 
 imran = Person("Imran", dt.date(2008,8,6), "Ubuntu")
