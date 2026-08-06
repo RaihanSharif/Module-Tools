1ffae146b075fd56a00e45c61727939ba317bcc7
Raihan Sharif <raihansharif92@gmail.com>
2026-08-06 12:02:51 +0100

    fix double

---
+++ sprint-5-exercises/fix_double.py
@@ -0,0 +1,7 @@
+def double(number):
+    # return number * 3
+    return number * 2. # the fix
+
+print(double(10))
+
+# bug: function is called double, but returns tripple of what is given as input.
