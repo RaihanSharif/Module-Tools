75ccf6d387cceb394146333fb8fc7963537fd1f2
Raihan Sharif <raihansharif92@gmail.com>
2026-08-06 17:33:33 +0100

    is_adult type check

---
+++ sprint-5-exercises/add_is_adult.py
@@ -0,0 +1,29 @@
+
+
+class Person:
+    def __init__(self, name: str, age: int, preferred_operating_system: str):
+        self.name = name
+        self.age = age
+        self.preferred_operating_system = preferred_operating_system
+
+
+
+imran = Person("Imran", 22, "Ubuntu")
+print(imran.name)
+# print(imran.address)
+
+eliza = Person("Eliza", 34, "Arch Linux")
+print(eliza.name)
+# print(eliza.address)
+
+def is_adult(person: Person) -> bool:
+    return person.age >= 18
+
+print(is_adult(imran))
+
+def is_developer(person: Person) -> bool:
+    return person.is_developer
+
+print(is_developer(imran))
+
+# As expected, there is an error because the is_developer attribute is not present in the Person class.
